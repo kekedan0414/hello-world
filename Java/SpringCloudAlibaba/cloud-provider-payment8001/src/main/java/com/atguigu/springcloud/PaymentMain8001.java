7fe5e6e09469acb40613ba606d72f75f421b0f55
kekedan0414 <xiajianle007.163.com>
2021-02-28 08:30:47 +0800

    springcloudalibaba

---
+++ Java/SpringCloudAlibaba/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ *
+ */
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
